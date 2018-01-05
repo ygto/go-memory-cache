a15e30a6062c310cf27a8545e03736924fe5d031
yiğit oztemel <yigit.oztemel@beyn.com.tr>
2018-01-05 12:51:38 +0300

    first init

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+)
+
+type memoryCache struct {
+	Data map[string]string
+	mux  sync.Mutex
+}
+
+func (c *memoryCache) Set(key string, val string) {
+	defer c.mux.Unlock()
+	c.mux.Lock()
+	c.Data[key] = val
+}
+func (c *memoryCache) Get(key string) (string, bool) {
+	defer c.mux.Unlock()
+	c.mux.Lock()
+	val, ok := c.Data[key]
+	return val, ok
+}
+func NewCache() *memoryCache {
+	c := memoryCache{}
+	c.Data = make(map[string]string)
+	return &c
+}
+
+func main() {
+
+	c := NewCache()
+	wg := sync.WaitGroup{}
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			key := fmt.Sprintf("name_%d", i)
+			val := fmt.Sprintf("%d", i)
+			c.Set(key, val)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	wg = sync.WaitGroup{}
+
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			key := fmt.Sprintf("name_%d", i)
+			if data, ok := c.Get(key); ok {
+				fmt.Println(data)
+			}
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
